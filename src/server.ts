0f419ed1f123e9de622d84180359f9d4938a4ec7
jonas souza <jonassouza305@yahoo.com.br>
2024-12-02 23:55:47 -0300

    --amend

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,8 @@ import cors from "cors";
 const helmet = require("helmet");
 import * as path from "path";
 import * as dotenv from "dotenv";
-const envPath = path.resolve(__dirname, "../../.env.development");
+const envPath = path.resolve(__dirname, "../.env.development");
 dotenv.config({ path: envPath });
-
 
 const port = process.env.PORT
 const app = express();
